94a5320b4222f0d9b64a5a06b2d8a78351c0326f
azkifr <90175598+azkifr@users.noreply.github.com>
2021-10-10 07:50:49 +0700

    Add files via upload

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public Text MyscoreText;
+    private int ScoreNum;
+    public void start()
+    {
+        ScoreNum = 0;
+        MyscoreText.text = "Score:" + ScoreNum;
+    }
+    public void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        transform.position = new Vector2(cursorPos.x, cursorPos.y);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Spawnable")
+        {
+            ScoreNum += 1;
+            Destroy(collision.gameObject);
+            MyscoreText.text = "Score: " + ScoreNum;
+        }
+        
+    }
+}
